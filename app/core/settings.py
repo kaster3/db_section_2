d842ab063406444e47e20c8d932d5b45eb6e5d68
Fedorov Semen <FedorovSemen28@yandex.ru>
2025-07-25 14:39:02 +0300

    config logger and db in settings

--- app/core/settings.py
+++ app/core/settings.py
@@ -1,6 +1,32 @@
+import os
 from pathlib import Path
+from typing import Literal
+from dataclasses import dataclass
 
 from dotenv import load_dotenv
 
-env_path = Path(__file__).parent.parent.parent / ".template.env"
-load_dotenv(env_path)
+
+@dataclass(slots=True)
+class Logs:
+    format: str
+    log_level: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"] = "WARNING"
+
+
+@dataclass(slots=True)
+class Postgresql:
+    url: str
+
+
+class Settings:
+    def __init__(self) -> None:
+        self._load_config()
+        self.logs = Logs(log_level=os.getenv("LOG_LEVEL"), format=os.getenv("FORMAT"))
+        self.db = Postgresql(url=os.getenv("DB_URL"))
+
+    @staticmethod
+    def _load_config() -> None:
+        env_path = Path(__file__).parent.parent.parent / ".template.env"
+        load_dotenv(env_path)
+
+
+settings = Settings()
